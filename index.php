b3d5273478dea7368e4cbf24b059e9a75becf09e
NraugSeev <106294520+NraugSeev@users.noreply.github.com>
2022-05-30 13:34:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+    @session_start();
+    unset($_SESSION['user_id']);
+    unset($_SESSION['fname']);
+    unset($_SESSION['lname']);
+	unset($_SESSION['image']);
+	unset($_SESSION['gender']);
+    unset($_SESSION['checked']);
+    session_destroy();
+    header("location:login/index.php");
+    exit;
+?>
